bee96c07945545c2f2708017089cacf33ffcc7f5
David Rohrbaugh <rohrbaugh.david17@libertycommon.org>
2015-05-20 17:16:37 -0600

    Assure that Caps Lock is off

--- CapsLockKill.bat
+++ CapsLockKill.bat
@@ -1,1 +1,4 @@
 taskkill /IM wscript.exe /F
+start /b /wait powershell.exe -nologo -WindowStyle Hidden -sta -command "IF ([console]::CapsLock -eq 'True'){$wsh = New-Object -ComObject WScript.Shell;$wsh.SendKeys('{CAPSLOCK}')}"
+taskkill /IM powershell.exe /F
+taskkill /IM wscript.exe /F
