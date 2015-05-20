b681bf25db12c9f83cf1f018a5783d73ab7c685c
David Rohrbaugh <rohrbaugh.david17@libertycommon.org>
2015-05-20 11:30:20 -0600

    Update color.bat

--- color.bat
+++ color.bat
@@ -1,12 +1,13 @@
 @echo off
-color -1
+:A
 color 0a
-:: color 1b
-:: color 2c
-:: color 3d
-:: color 4e
-:: color 5f
-:: color 6a
-:: color 7b
-:: color 8c
-:: color 9d
+color 1b
+color 2c
+color 3d
+color 4e
+color 5f
+color 6a
+color 7b
+color 8c
+color 9d
+goto:A
