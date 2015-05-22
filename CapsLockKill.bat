58b87603e078a2ea0466bced6072176c4d01bd8c
David Rohrbaugh <rohrbaugh.david17@libertycommon.org>
2015-05-22 14:51:31 -0600

    Add @echo off line to CapsLockKill.bat

--- CapsLockKill.bat
+++ CapsLockKill.bat
@@ -1,3 +1,4 @@
+@echo off
 taskkill /IM wscript.exe /F
 start /b /wait powershell.exe -nologo -WindowStyle Hidden -sta -command "IF ([console]::CapsLock -eq 'True'){$wsh = New-Object -ComObject WScript.Shell;$wsh.SendKeys('{CAPSLOCK}')}"
 taskkill /IM powershell.exe /F
