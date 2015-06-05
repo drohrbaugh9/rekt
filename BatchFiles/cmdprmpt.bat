9b8a28a339e60e295f568c009a1e0504b922c730
David Rohrbaugh <rohrbaugh.david17@libertycommon.org>
2015-06-05 15:02:30 -0600

    Update cmdprompt.bat

--- BatchFiles/cmdprmpt.bat
+++ BatchFiles/cmdprmpt.bat
@@ -3,12 +3,14 @@ echo Microsoft Windows [Version 6.1.7601]
 echo Copyright (c) 2009 Microsoft Corporation. All rights reserved.
 :A
 echo.
-set /p command="C:\>"
-IF %command%==git (
-	type git.txt
-) ELSE (
-	echo '%command%' is not recognized as an internal or external command,
-	echo operable program or batch file.
-)
+SET /p command="C:\>"
+echo '%command%' is not recognized as an internal or external command,
+echo operable program or batch file.
+REM SET /a rand=%random% %%10 +1
+REM IF %rand% EQU 2 color A
+REM IF %rand% EQU 3 (
+REM 	You have been hacked...
+REM 	timeout /t 1 /nobreak >nul
+REM 	exit
+REM )
 goto A
-REM Yes I did just use a goto
