93a4b122b8605a0e8d4149cbe0d370ca37ed34e7
Rohrbaugh <drohrbaugh9@users.noreply.github.com>
2015-06-16 13:34:53 -0600

    Change comments from REM to ::

--- BatchFiles/cmdprmpt.bat
+++ BatchFiles/cmdprmpt.bat
@@ -3,14 +3,14 @@ echo Microsoft Windows [Version 6.1.7601]
 echo Copyright (c) 2009 Microsoft Corporation. All rights reserved.
 :A
 echo.
-SET /p command="C:\>"
+set /p command="C:\>"
 echo '%command%' is not recognized as an internal or external command,
 echo operable program or batch file.
-REM SET /a rand=%random% %%10 +1
-REM IF %rand% EQU 2 color A
-REM IF %rand% EQU 3 (
-REM 	You have been hacked...
-REM 	timeout /t 5 /nobreak >nul
-REM 	exit
-REM )
+:: SET /a rand=%random% %%10 +1
+:: IF %rand% EQU 2 color A
+:: IF %rand% EQU 3 (
+:: 	You have been hacked...
+:: 	timeout /t 5 /nobreak >nul
+:: 	exit
+:: )
 goto A
