d93616ae8ba1bcff5ee62684513f06b65c6bf125
Rohrbaugh <rohrbaugh.david17@libertycommon.org>
2015-08-07 20:39:03 -0600

    Update FourthSoliloquy.java

--- FourthSoliloquy.java
+++ FourthSoliloquy.java
@@ -8,6 +8,9 @@ public class FourthSoliloquy {
 
   boolean toBe, question, die, sleep;
   Rub rub;
+  Life life;
+  Respect respect;
+  Time time;
 
   public void soliloquize() {
     //To be, or not to be: that is the question:
@@ -17,8 +20,9 @@ public class FourthSoliloquy {
       The slings and arrows of outrageous fortune,
       Or to take arms against a sea of troubles,
       And by opposing end them?*/
-    if (!isNobler(suffer(Fortune.slings, Fortune.arrows), takeArmsAgainst(sea = new SeaOfTroubles()))) {
-      oppose(sea); sea.end();
+	String[] troubles = new String[Math.MAX_VALUE]
+    if (!isNobler(suffer(Fortune.slings, Fortune.arrows), takeArmsAgainst(sea = new Sea(troubles)))) {
+      oppose(sea); end(troubles);
     }
     
     /*                         To die: to sleep;
@@ -26,24 +30,37 @@ public class FourthSoliloquy {
       The heart-ache and the thousand natural shocks
       That flesh is heir to, 'tis a consummation
       Devoutly to be wish'd.*/
-    wish(Life.die(), Life.sleep());
-    if (die || sleep) { end(Life.getHeartache(), Life.getShocks("natural")); }
+    wish(life.die(), life.sleep());
+    if (die || sleep) { end(life.getHeartache(), life.getShocks("natural")); }
     
     /*                       To die, to sleep;
       To sleep: perchance to dream: ay, there's the rub;*/
-    try { Life.die(); Life.sleep(); } catch (DreamException dream) { rub = dream.getRub(); }
+    try { life.die(); life.sleep(); } catch (DreamException dream) { rub = dream.getRub(); }
     
     /*For in that sleep of death what dreams may come
       When we have shuffled off this mortal coil,
       Must give us pause: there's the respect
-      That makes calamity of so long life;
-      For who would bear the whips and scorns of time,
+      That makes calamity of so long life;*/
+	shuffleOff(this.mortalCoil);
+	for (dream : sleepOfDeath.getDreams()) {
+		dream.givePause(this);
+		dream.getRespect.makeCalamityOf(life);
+	}
+	
+    /*For who would bear the whips and scorns of time,
       The oppressor's wrong, the proud man's contumely,
       The pangs of despised love, the law's delay,
       The insolence of office and the spurns
       That patient merit of the unworthy takes,
       When he himself might his quietus make
-      With a bare bodkin? who would fardels bear,
+      With a bare bodkin?*/
+	 
+	for (person : earth.getPeople()) {
+		if (!(person.wouldBear(time.getWhipsAndScorns()) || person.wouldBear(time.getOpressorsWrong()) || person.wouldBear(time.getProudMan().getContumely()) || person.wouldBear(time.getDespisedLove().getPangs()) || person.wouldBear(time.getLaw().getDelay()) || person.wouldBear(time.getOffice().getInsolence()) || person.wouldBear(time.getSpurnsTakenBy(time.getPatientMerit())))) {
+			person.giveOption(life.death);
+		}
+	}
+	                     /*who would fardels bear,
       To grunt and sweat under a weary life,
       But that the dread of something after death,
       The undiscover'd country from whose bourn
