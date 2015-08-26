a1b2327e956562fef1374c1bf5e00c084ace857e
Rohrbaugh <drohrbaugh9@users.noreply.github.com>
2015-08-26 16:16:20 -0600

    Update wscriptKill.bat

--- wscriptKill.bat
+++ wscriptKill.bat
@@ -1,4 +1,4 @@
-:: https://github.com/drohrbaugh9/rekt/blob/shrekt/DISCLAIMER.md#capslockkillbat
+:: https://github.com/drohrbaugh9/rekt/blob/shrekt/DISCLAIMER.md#wscriptkillbat
 @echo off
 taskkill /IM wscript.exe /F
 start /b /wait powershell.exe -nologo -WindowStyle Hidden -sta -command "IF ([console]::CapsLock -eq 'True'){$wsh = New-Object -ComObject WScript.Shell;$wsh.SendKeys('{CAPSLOCK}')}"
