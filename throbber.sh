f9487216af5365510fcbbf31ddeef9dd5eb02e23
David Rohrbaugh <rohrbaugh.david17@libertycommon.org>
2016-10-15 16:06:45 -0600

    Modify loading.sh, rename as throbber.sh

---
+++ throbber.sh
@@ -0,0 +1,12 @@
+clear
+while :
+do
+  echo -e "\x1b[1;1H[-]\x1b[0H"
+  sleep 0.5
+  echo -e "\x1b[1;1H[\\]\x1b[0H"
+  sleep 0.5
+  echo -e "\x1b[1;1H[|]\x1b[0H"
+  sleep 0.5
+  echo -e "\x1b[1;1H[/]\x1b[0H"
+  sleep 0.5
+done
