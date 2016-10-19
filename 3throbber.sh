88021173141dc63c7365e8fdb93698d57a029f91
David Rohrbaugh <rohrbaugh.david17@libertycommon.org>
2016-10-19 10:29:19 -0600

    Add 3throbber.sh

---
+++ 3throbber.sh
@@ -0,0 +1,16 @@
+clear
+while :
+do
+	echo -e "\x1b[1;1H####\n####\n####\n####\n    \n    "
+	sleep 0.5
+	echo -e "\x1b[1;1H##  \n####\n####\n####\n  ##\n    "
+	sleep 0.5
+	echo -e "\x1b[1;1H##  \n##  \n####\n####\n  ##\n  ##"
+	sleep 0.5
+	echo -e "\x1b[1;1H ## \n ## \n####\n####\n ## \n ## "
+	sleep 0.5
+	echo -e "\x1b[1;1H  ##\n  ##\n####\n####\n##  \n##  "
+	sleep 0.5
+	echo -e "\x1b[1;1H  ##\n####\n####\n####\n##  \n    "
+	sleep 0.5
+done
