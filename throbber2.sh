f4acc73787ce9fa34740d0d144a724475e035936
Rohrbaugh <drohrbaugh9@users.noreply.github.com>
2016-10-18 23:41:30 -0600

    Update throbber2.sh

--- throbber2.sh
+++ throbber2.sh
@@ -5,4 +5,22 @@ do
   sleep 0.5
   echo -e "\x1b[1;1H o     \n  o   o\n   \x1b[0;31mo\x1b[0m   "
   sleep 0.5
+  echo -e "\x1b[1;1H  o    \n  o   o\n  \x1b[0;31mo\x1b[0m    "
+  sleep 0.5
+  echo -e "\x1b[1;1H   o   \n  o   o\n \x1b[0;31mo\x1b[0m     "
+  sleep 0.5
+  echo -e "\x1b[1;1H       \n\x1b[0;31mo\x1b[0m o o o\n       "
+  sleep 0.5
+  echo -e "\x1b[1;1H \x1b[0;31mo\x1b[0m     \n  o   o\n   o   "
+  sleep 0.5
+  echo -e "\x1b[1;1H  \x1b[0;31mo\x1b[0m    \n  o   o\n  o    "
+  sleep 0.5
+  echo -e "\x1b[1;1H   \x1b[0;31mo\x1b[0m   \n  o   o\n o     "
+  sleep 0.5
+  echo -e "\x1b[1;1H       \no o \x1b[0;31mo\x1b[0m o\n       "
+  sleep 0.5
+  echo -e "\x1b[1;1H     o \no   \x1b[0;31mo\x1b[0m  \n   o   "
+  sleep 0.5
+  echo -e "\x1b[1;1H   o   \no   \x1b[0;31mo\x1b[0m  \n     o "
+  sleep 0.5
 done
