2270511fa00c0f59b367db8d2bd2fa4394cfa35c
David Rohrbaugh <rohrbaugh.david17@libertycommon.org>
2016-10-19 10:39:51 -0600

    "Optimize" throbber.sh

--- throbber.sh
+++ throbber.sh
@@ -1,12 +1,13 @@
 clear
+echo "[ ]"
 while :
 do
-  echo -e "\x1b[1;1H[-]\x1b[0H"
+  echo -e "\x1b[1;2H-"
   sleep 0.5
-  echo -e "\x1b[1;1H[\\]\x1b[0H"
+  echo -e "\x1b[1;2H\\"
   sleep 0.5
-  echo -e "\x1b[1;1H[|]\x1b[0H"
+  echo -e "\x1b[1;2H|"
   sleep 0.5
-  echo -e "\x1b[1;1H[/]\x1b[0H"
+  echo -e "\x1b[1;2H/"
   sleep 0.5
 done
