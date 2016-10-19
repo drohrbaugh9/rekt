e480e5887e890b0c3ac4a026ec8bbc52fa6377b7
David Rohrbaugh <rohrbaugh.david17@libertycommon.org>
2016-10-19 10:59:31 -0600

    Add 4throbber.sh

---
+++ 4throbber.sh
@@ -0,0 +1,20 @@
+clear
+while :
+do
+	echo -e "\x1b[1;1H\x1b[0;31m0 \x1b[0m\n  "
+	sleep 1
+	echo -e "\x1b[1;2H\x1b[0;33m0\x1b[0m\n"
+	sleep 1
+	echo -e "\x1b[2;2H\x1b[0;32m0\x1b[0m"
+	sleep 1
+	echo -e "\x1b[2;1H\x1b[0;34m0\x1b[0;32m0\x1b[0m"
+	sleep 1
+	echo -e "\x1b[2;1H \x1b[0;32m0\x1b[0m" 
+	sleep 1
+	echo -e "\x1b[2;2H "
+	sleep 1
+	echo -e "\x1b[1;2H \n"
+	sleep 1
+	echo -e "\x1b[1;1H \n"
+	sleep 1
+done
