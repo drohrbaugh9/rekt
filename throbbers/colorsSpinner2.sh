87217eb8b7d2cb5d950ae948f6fefde1e7f59869
drohrbaugh <rohrbaugh.david17@libertycommon.org>
2016-11-29 11:53:32 -0700

    Create colorsSpinner2.sh

---
+++ throbbers/colorsSpinner2.sh
@@ -0,0 +1,17 @@
+clear
+t=0.5
+while :
+do
+	echo -e "\x1b[1;1H\x1b[0;31m#\x1b[0m"
+	sleep $t
+	echo -e "\x1b[1;1H\x1b[0;33m#\x1b[0m"
+	sleep $t
+	echo -e "\x1b[1;1H\x1b[0;32m#\x1b[0m"
+	sleep $t
+	echo -e "\x1b[1;1H\x1b[0;36m#\x1b[0m"
+	sleep $t
+	echo -e "\x1b[1;1H\x1b[0;34m#\x1b[0m"
+	sleep $t
+	echo -e "\x1b[1;1H\x1b[0;35m#\x1b[0m"
+	sleep $t
+done
