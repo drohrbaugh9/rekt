1fcbb95bcae0cf61738a4d327742f74249c2df74
David Rohrbaugh <rohrbaugh.david17@libertycommon.org>
2016-12-01 10:30:28 -0700

    Add stringLoopTest, binary16

---
+++ throbbers/binary16.sh
@@ -0,0 +1,37 @@
+clear
+t=0.5
+while :
+do
+	echo -e "\x1b[1;1H    "
+	sleep $t
+	echo -e "\x1b[1;1H   #"
+	sleep $t
+	echo -e "\x1b[1;1H  # "
+	sleep $t
+	echo -e "\x1b[1;1H  ##"
+	sleep $t
+	echo -e "\x1b[1;1H #  "
+	sleep $t
+	echo -e "\x1b[1;1H # #"
+	sleep $t
+	echo -e "\x1b[1;1H ## "
+	sleep $t
+	echo -e "\x1b[1;1H ###"
+	sleep $t
+	echo -e "\x1b[1;1H#   "
+	sleep $t
+	echo -e "\x1b[1;1H#  #"
+	sleep $t
+	echo -e "\x1b[1;1H# # "
+	sleep $t
+	echo -e "\x1b[1;1H# ##"
+	sleep $t
+	echo -e "\x1b[1;1H##  "
+	sleep $t
+	echo -e "\x1b[1;1H## #"
+	sleep $t
+	echo -e "\x1b[1;1H### "
+	sleep $t
+	echo -e "\x1b[1;1H####"
+	sleep $t
+done
