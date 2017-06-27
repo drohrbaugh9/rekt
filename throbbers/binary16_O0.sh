2ea250fea0dce579044a0cda389dd9c4623dac8f
David Rohrbaugh <david.rohrbaugh@hpe.com>
2017-06-27 10:56:46 -0600

    Copy binary16, replace " " with O and # with 0

---
+++ throbbers/binary16_O0.sh
@@ -0,0 +1,37 @@
+clear
+t=0.5
+while :
+do
+	echo -e "\x1b[1;1HOOOO"
+	sleep $t
+	echo -e "\x1b[1;1HOOO0"
+	sleep $t
+	echo -e "\x1b[1;1HOO0O"
+	sleep $t
+	echo -e "\x1b[1;1HOO00"
+	sleep $t
+	echo -e "\x1b[1;1HO0OO"
+	sleep $t
+	echo -e "\x1b[1;1HO0O0"
+	sleep $t
+	echo -e "\x1b[1;1HO00O"
+	sleep $t
+	echo -e "\x1b[1;1HO000"
+	sleep $t
+	echo -e "\x1b[1;1H0OOO"
+	sleep $t
+	echo -e "\x1b[1;1H0OO0"
+	sleep $t
+	echo -e "\x1b[1;1H0O0O"
+	sleep $t
+	echo -e "\x1b[1;1H0O00"
+	sleep $t
+	echo -e "\x1b[1;1H00OO"
+	sleep $t
+	echo -e "\x1b[1;1H00O0"
+	sleep $t
+	echo -e "\x1b[1;1H000O"
+	sleep $t
+	echo -e "\x1b[1;1H0000"
+	sleep $t
+done
